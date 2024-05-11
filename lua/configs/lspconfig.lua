47b65b4dd833ecefe5bc9501975d7a46d9e3c6c4
Jason Wang <wangborong@cdjrlc.com>
2024-05-11 09:40:16 +0800

    Update clangd lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,42 +1,39 @@
+local lspconfig = require("lspconfig")
 
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
-local servers = { 'clangd', 'rust_analyzer', 'pyright',
-                  'bashls','verible', 'texlab' }
+local defconf = {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+-- If on_attach, on_init and capabilities don't meet your needs,
+-- You can override it here.
+local servers = {
+  clangd = {
+    cmd = {
+      "clangd",
+      "--header-insertion=never",
+      "-j", string.gsub(vim.fn.system('nproc'), "\n", ""),
+      "--completion-style=detailed",
+      "--function-arg-placeholders",
+      "--rename-file-limit=0",
+      "--background-index",
+      "--background-index-priority=normal",
+    },
+  },
+  rust_analyzer = {},
+  pyright = {},
+  bashls = {},
+  verible = {},
+  texlab = {},
+}
 
 -- lsps with default config
-for _, lsp in ipairs(servers) do
-  if lsp == "clangd" then
-    lspconfig[lsp].setup {
-      on_attach = on_attach,
-      on_init = on_init,
-      capabilities = capabilities,
-      cmd = {
-        "clangd",
-        "--header-insertion=never",
-        "-j", string.gsub(vim.fn.system('nproc'), "\n", ""),
-        "--completion-style=detailed",
-        "--function-arg-placeholders",
-        "--rename-file-limit=0",
-        "--background-index",
-        "--background-index-priority=normal",
-      },
-    }
-  else
-    lspconfig[lsp].setup {
-      on_attach = on_attach,
-      on_init = on_init,
-      capabilities = capabilities,
-    }
-  end
+for lsp, optconf in pairs(servers) do
+  local conf = vim.tbl_deep_extend("force", defconf, optconf)
+  lspconfig[lsp].setup(conf)
 end
-
--- typescript
--- lspconfig.tsserver.setup {
---   on_attach = on_attach,
---   on_init = on_init,
---   capabilities = capabilities,
--- }

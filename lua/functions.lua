ff74d4dff4a472e6b00365951cd2d5ebdd29d733
Jason Wang <wangborong@cdjrlc.com>
2024-05-24 19:35:48 +0800

    Add pandoc converter

--- lua/functions.lua
+++ lua/functions.lua
@@ -17,3 +17,26 @@ function ReplaceLineWithCmd(cmd, lnum)
     i = i + 1
   end
 end
+
+-- convert markdown text which is from clipboard to latex text with pandoc
+function TextConvert(f, t)
+  local handle = io.popen("xclip -out | pandoc --listings --from=" .. f .. " --to=" .. t)
+  local text
+  if handle ~= nil then
+    text = handle:read("*a")
+    handle:close()
+    local lines = {}
+    for line in text:gmatch("([^\n]*)\n?") do
+      table.insert(lines, line)
+    end
+    vim.api.nvim_put(lines, "", false, true)
+  end
+end
+
+function Md2Tex()
+  TextConvert("markdown", "latex")
+end
+
+function Tex2Md()
+  TextConvert("latex", "markdown")
+end

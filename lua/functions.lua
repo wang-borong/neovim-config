0779cb51f42e4844cdcde129405b36ed1dc696ab
Jason Wang <wang_borong@163.com>
2024-05-26 13:20:52 +0800

    Add convert selected text function

--- lua/functions.lua
+++ lua/functions.lua
@@ -19,8 +19,14 @@ function ReplaceLineWithCmd(cmd, lnum)
 end
 
 -- convert markdown text which is from clipboard to latex text with pandoc
-function TextConvert(f, t)
-  local handle = io.popen("xclip -out | pandoc --listings --from=" .. f .. " --to=" .. t)
+local function TextConvert(f, t, sel)
+  local handle
+  if sel == true then
+    local vstext = require("helper").get_visual_selection()
+    handle = io.popen(('echo "%s" | pandoc --listings --from=%s --to=%s'):format(vstext, f, t))
+  else
+    handle = io.popen("xclip -out | pandoc --listings --from=" .. f .. " --to=" .. t)
+  end
   local text
   if handle ~= nil then
     text = handle:read("*a")
@@ -28,6 +34,11 @@ function TextConvert(f, t)
     local lines = {}
     for line in text:gmatch("([^\n]*)\n?") do
       table.insert(lines, line)
+    end
+    if sel == true then
+      local s_start = vim.fn.getpos("'<")
+      local s_end = vim.fn.getpos("'>")
+      vim.cmd(("%d,%dd"):format(s_start[2] - 1, s_end[2]))
     end
     vim.api.nvim_put(lines, "", false, true)
   end
@@ -39,4 +50,12 @@ end
 
 function Tex2Md()
   TextConvert("latex", "markdown")
+end
+
+function VMd2Tex()
+  TextConvert("markdown", "latex", true)
+end
+
+function VTex2Md()
+  TextConvert("latex", "markdown", true)
 end

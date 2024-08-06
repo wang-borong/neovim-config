f695261b7c23fb207b87a423131ee92e581c8781
Jason Wang <wangborong@cdjrlc.com>
2024-08-06 14:30:33 +0800

    Update autocmds

--- lua/options.lua
+++ lua/options.lua
@@ -25,35 +25,44 @@ o.mouse = ""
 
 opt.iskeyword:append("-")
 
-autocmd("FileType", {
-  pattern = { "lua", "markdown", "tex" },
-  callback = function()
-    o.shiftwidth = 2
-    o.tabstop = 2
-  end,
-})
+local autocmds = {
+  [ { "FileType" } ] = { {
+      pattern = { "lua", "markdown", "tex" },
+      callback = function()
+        o.shiftwidth = 2
+        o.tabstop = 2
+      end,
+    }, {
+      pattern = { "python" },
+      callback = function()
+        o.textwidth = 120
+      end,
+    }, {
+      pattern = { "rust" },
+      callback = function ()
+        o.textwidth = 100
+      end
+    }, {
+      pattern = { "kconfig", "make" },
+      callback = function()
+        -- we need real tabs
+        o.expandtab = false
+      end,
+    },
+  },
+  [ { "BufEnter", "BufWinEnter" } ] = { {
+      pattern = { "*.c", "*.h" },
+      callback = function()
+        o.tabstop = 8
+        o.shiftwidth = 8
+        o.expandtab = false
+      end,
+    },
+  }
+}
 
-autocmd("FileType", {
-  pattern = { "python" },
-  callback = function()
-    o.expandtab = true
-    o.textwidth = 120
-  end,
-})
-
-autocmd({"BufEnter", "BufWinEnter"}, {
-  pattern = { "*.c", "*.h" },
-  callback = function()
-    o.tabstop = 8
-    o.shiftwidth = 8
-    o.expandtab = false
-  end,
-})
-
-autocmd("FileType", {
-  pattern = { "kconfig", "make" },
-  callback = function()
-    -- we need real tabs
-    o.expandtab = false
-  end,
-})
+for k,vs in pairs(autocmds) do
+  for _,v in ipairs(vs) do
+    autocmd(k, v)
+  end
+end
